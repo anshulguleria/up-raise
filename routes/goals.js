df02b785cadf430c3e83f0459900e8f6470fcee2
Varun Jain <vjain@tavisca.com>
2013-10-12 17:23:28 +0530

    added self view for goals

--- routes/goals.js
+++ routes/goals.js
@@ -4,5 +4,6 @@
  */
 
 exports.list = function(req, res){
-  res.render('goals', { title: 'Goals' ,  user: req.user });
+  res.render('goals', { title: 'Goals' ,  user: req.user, employee: req.user, 
+  	cycle: { startDate: 'Apr 2013', endDate: 'Sept 2013' }, view: 'self' });
 };
