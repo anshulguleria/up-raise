7e9555601649dd645a389fcc821c9aaa4ccadf10
Varun Jain <vjain@tavisca.com>
2013-10-12 17:40:46 +0530

    added goals template

--- routes/goals.js
+++ routes/goals.js
@@ -5,5 +5,39 @@
 
 exports.list = function(req, res){
   res.render('goals', { title: 'Goals' ,  user: req.user, employee: req.user, 
-  	cycle: { startDate: 'Apr 2013', endDate: 'Sept 2013' }, view: 'self' });
+  	goals: {
+  		cycle: { startDate: 'Apr 2013', endDate: 'Sept 2013' },
+		items:[ [
+				'1',
+				'Company Roadmap',
+				'Organization',
+				'Ensure meetings for company roadmap occurs bi-weekly. Track and guide Product management organization to achieve goals set. Track and guide Services organization to achieve goals set',
+				'10',
+				'',
+				''
+				],
+				[
+				'2',
+				'Company Goals',
+				'Organization',
+				'Build company goals for year 2010-11. Publish goals to sales, hr, admin, products and services teams',
+				'10',
+				'',
+				''
+				],
+				[
+				'3',
+				'Company Finance',
+				'Organization',
+				'Setup company financial report publish to stake holders. Increase per person revenue by 10%.',
+				'10',
+				'',
+				''
+				] 
+			],
+		status: 'pending'
+
+				
+  	}
+  	, view: 'self' });
 };
